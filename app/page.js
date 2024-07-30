6c905b09120a337ef510c651d343f04a50b60401
Macmilan24 <cpt.macmilan24@gmail.com>
2024-07-30 13:20:28 +0300

    worked only on basic ui

--- app/page.js
+++ app/page.js
@@ -1,15 +1,57 @@
-import { Box, Stack } from "@mui/material";
+import { Box, Stack, Typography } from "@mui/material";
+
+const item = [
+  "tomato",
+  "potato",
+  "onion",
+  "garlic",
+  "ginger",
+  "carrot",
+  "lettuce",
+  "kale",
+];
 
 export default function Home() {
   return (
     <Box
       width="100vw"
       height="100vh"
-      display="flex"
-      justifyContent="center"
-      alignItems="center"
+      display={"flex"}
+      justifyContent={"center"}
+      alignItems={"center"}
+      flexDirection={"column"}
     >
-      <Stack width="800px" height="600px"></Stack>
+      <Box border={"1px solid #333"}>
+        <Box
+          width="800px"
+          height="100px"
+          bgcolor={"#ADD8E6"}
+          display={"flex"}
+          justifyContent={"center"}
+          alignItems={"center"}
+        >
+          <Typography variant={"h2"} color={"#333"} textAlign={"center"}>
+            Pantry
+          </Typography>
+        </Box>
+        <Stack width="800px" height="200px" spacing={2} overflow={"auto"}>
+          {item.map((i) => (
+            <Box
+              key={i}
+              width="100%"
+              height="100px"
+              display={"flex"}
+              justifyContent={"center"}
+              alignItems={"center"}
+              bgcolor={"#f0f0f0f0"}
+            >
+              <Typography variant={"h3"} color={"#333"} textAlign={"center"}>
+                {i.charAt(0).toUpperCase() + i.slice(1)}
+              </Typography>
+            </Box>
+          ))}
+        </Stack>
+      </Box>
     </Box>
   );
 }
